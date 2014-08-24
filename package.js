715bc36c84e05983b92481950f6a098ce761d08d
Pahan Sarathchandra <pahan123@gmail.com>
2014-08-24 15:32:46 +0530

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.add_files('pagination.js', 'client');
-  api.export('Pagination');
+  api.export('Pagination', 'client');
 });
 
 Package.on_test(function (api) {
